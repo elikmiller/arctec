643e867226ff21283fafe91867c873f4ffc4367c
Eli K Miller <eli@zbook>
2016-11-01 16:33:47 -0500

    Added bootstrap 4

--- src/App.js
+++ src/App.js
@@ -5,17 +5,49 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container-fluid">
+        <Header />
+        <Gallery />
+        <Footer />
       </div>
     );
   }
 }
 
+class Header extends Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <a className="navbar-brand" href="#">ArcTec</a>
+        <ul className="nav navbar-nav float-xs-right">
+          <li className="nav-item">
+            <a className="nav-link" href="#">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Gallery extends Component {
+  render() {
+    return (
+      <div className="Gallery">
+        Gallery
+      </div>
+    );
+  }
+}
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+        Footer
+      </div>
+    );
+  }
+}
 export default App;
