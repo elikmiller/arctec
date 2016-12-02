489e556a089f2c91c5bae546375c7e30b5c9de69
Eli K Miller <eli@zbook>
2016-12-02 14:48:22 -0600

    Added simple bootstrap layout grid to gallery component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import mockup from '../public/img/mockup.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App container-fluid">
+      <div className="App container">
         <Header />
         <Gallery />
         <Footer />
@@ -18,7 +19,7 @@ class Header extends Component {
   render() {
     return (
       <nav className="navbar">
-        <a className="navbar-brand" href="#"><img src={logo} /></a>
+        <a className="navbar-brand" href="#"><img src={logo} alt="ArcTec" /></a>
         <ul className="nav navbar-nav float-xs-right">
           <li className="nav-item">
             <a className="nav-link" href="#">About</a>
@@ -32,11 +33,86 @@ class Header extends Component {
   }
 }
 
+class Photo extends Component {
+  render() {
+    return (<p>Hi im a photo component</p>);
+  }
+}
+
 class Gallery extends Component {
   render() {
     return (
       <div className="Gallery">
-        Gallery
+        <div className="row">
+          <div className="col-md-8">
+            <div className="row">
+              <div className="col-md-6">
+                <Photo />
+              </div>
+              <div className="col-md-6">
+                <p>img 02</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <p>img 03</p>
+              </div>
+              <div className="col-md-6">
+                <p>img 04</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <p>img 05</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-8">
+            <p>img 06</p>
+          </div>
+          <div className="col-md-4">
+            <p>img 07</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <p>img 08</p>
+          </div>
+          <div className="col-md-8">
+            <p>img 09</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <p>img 10</p>
+          </div>
+          <div className="col-md-4">
+            <div className="row">
+              <div className="col-md-12">
+                <p>img 11</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <p>img 12</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <p>img 13</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <p>img 14</p>
+          </div>
+          <div className="col-md-4">
+            <p>img 15</p>
+          </div>
+          <div className="col-md-4">
+            <p>img 16</p>
+          </div>
+        </div>
       </div>
     );
   }
